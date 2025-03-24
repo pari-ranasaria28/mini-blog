8045418f133b8d9101180645819f297ce809864b
pari-ranasaria28 <pari.ranasaria@gmail.com>
2025-03-24 23:15:53 +0530

    changes for vercel

--- project2_mini_blog/settings.py
+++ project2_mini_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-53t3(*3kwo@p2ftll1atpou-q*d-i8+%de)p$go(&g!m=a$160'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
